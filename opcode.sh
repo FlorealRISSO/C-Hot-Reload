c04e6a5489a0d2211c7e4ccfc971d8e8f4d34fff
ghuter <ghuter@disroot.org>
2023-11-14 08:43:48 +0100

    2 other version of opcode.sh

--- opcode.sh
+++ opcode.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
-FILE=$1
-FUN=$2
+f=$1
+fun=$2
 
-objdump --disassemble=$FUN $FILE | grep ' *[0-9a-f]:' | sed 's/ *[0-9a-f]*:\t//' | sed 's/   *.*//' | tr -d '\n' | tr -d ' '
+objdump --disassemble=$fun $f
+
+# objdump --disassemble=$fun $f | grep ' *[0-9a-f]:' | sed 's/ *[0-9a-f]*:\t//' | sed 's/   *.*//' | tr -d '\n' | tr -d ' '
+
+# objdump --disassemble="$fun" "$f" |
+	# sed '
+		# /^ *[0-9a-f]:/!d
+		# s/.*\t\(.*\)\t.*/\1/
+		# s/[ \t]//g
+	# ' | paste -s -d ''
+
+objdump --disassemble="$fun" "$f" |
+	sed '
+		/^ *[0-9a-f]:/!d
+		:l
+		s/.*\t\(.*\)\t.*/\1/
+		s/[ \t]//g
+		x; G
+		s/\n//
+		$p
+		h; d
+		t l
+	'
+
